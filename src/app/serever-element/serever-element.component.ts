cef1e3de837de77cf0c49e0f1af960751c599904
anil suryavanshi <thakurinfotech@gmail.com>
2017-08-12 21:46:56 +0530

    hack life

--- src/app/serever-element/serever-element.component.ts
+++ src/app/serever-element/serever-element.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, 
+  OnInit, 
+  Input,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  SimpleChanges
+
+} from '@angular/core';
 
 @Component({
   selector: 'app-serever-element',
   templateUrl: './serever-element.component.html',
   styleUrls: ['./serever-element.component.css']
 })
-export class SereverElementComponent implements OnInit {
+export class SereverElementComponent implements OnInit ,OnChanges, DoCheck,AfterContentInit {
   @Input('srvElement') element:{type:string,name:string,content:string};
-  constructor() { }
+  @Input() name;
+  constructor() { 
+    console.log('constructor called ');
+  }
 
   ngOnInit() {
+    console.log('ngon init called');
   }
+  ngOnChanges(changes:SimpleChanges){
+    console.log('ngon changes called'+changes);
+  }
+  ngDoCheck(){
+    console.log('ng Do checks called');
+  }
+
+  ngAfterContentInit(){
+    console.log('ng After Content init called');
+  }
+
+  
 
 }
